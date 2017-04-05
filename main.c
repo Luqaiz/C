9e825a368673f91a7891fb3c98dca070b8d2e723
Luqaiz <romerolucas28@gmail.com>
2017-04-05 12:23:09 -0300

    ejercicio 5 completar

--- main.c
+++ main.c
@@ -10,25 +10,40 @@ int main()
     float salarioAux;
     int edad [MAX];
     int i;
+    int j;
     int mayor;
+    int encontrado;
+    int legajoAux;
+    int edadAux;
 
     for (i=0 ; i<MAX ; i++)
     {
         printf ("Ingrese un numero de legajo: ");
-        scanf ("%d", &legajo[i]);
+        scanf ("%d", &legajoAux);
 
         printf ("Ingrese un salario: ");
         scanf ("%f", &salarioAux);
 
         printf ("ingrese una edad: ");
-        scanf ("%d", &edad[i]);
+        scanf ("%d", &edadAux);
 
         salario [i] = salarioAux;
+        edad [i] = edadAux;
+        legajo [i] = legajoAux;
+
+        salario [j] = salarioAux;
+        edad [j] = edadAux;
+        legajo [j] = legajoAux;
 
     }
+    if (encontrado == 0)
+        {
+            printf ("registro no encontrado");
+        }
+
         system ("cls");
 
-    printf("\n\n Legajo\tEdad\tSalario");
+    printf("\n\nLegajo\tEdad\tSalario");
 
 
 
@@ -41,7 +56,38 @@ int main()
             mayor = salario [i];
         }
 
+        if (legajoAux == legajo [i])
+        {
+            printf ("\n %d\t%d\t%f \n", legajo[i],edad[i],salario[i]);
+            encontrado = 1;
+        }
+
     }
+
+    for (i=0 ; i< MAX-1 ; i++)
+    {
+        for (j=i+1 ; j<MAX ; j++)
+            {
+                if (legajo[i] > legajo [j])
+                {
+                    legajoAux = legajo [i];
+                    legajo [i] = legajo [j];
+                    legajo [j] = legajoAux;
+
+                    edadAux = edad [i];
+                    edad [i] = edad [j];
+                    edad [j] = edadAux;
+
+                    salarioAux = salario [i];
+                    salario [i] = salario[j];
+                    salario [j] = salarioAux;
+
+                }
+            }
+
+    }
+
+
 
         printf ("el salario mayor es %d: ", mayor);
 
