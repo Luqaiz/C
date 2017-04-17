38fd03a15ee0f8fe00073b23099283202d3b91a8
Luqaiz <romerolucas28@gmail.com>
2017-04-17 12:31:02 -0300

    clase 8

--- main.c
+++ main.c
@@ -1,96 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 3
 
-
-int main()
+typedef struct
 {
-    int legajo [MAX];
-    float salario [MAX];
-    float salarioAux;
-    int edad [MAX];
+    int dia;
+    int mes;
+    int anio;
+
+}eFecha;
+
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    char apellido[30];
+    float promedio;
+    eFecha fnac;
+
+}eAlumno;
+
+void mostrarAlumno (eAlumno);
+void mostrarCurso (curso, int)
+
+int main ()
+{
+    /*eAlumno unAlumno;*/
+    eAlumno curso [] = {{1111,"Juan","Perez",7,{4,11,1995}},{2222,"Jorge","Suarez",8.5,{12,8,1996}},{3333,"Roberto","Pepazo",8,{4,4,1998}}}; /*= {827,"Juan","Perez",9,{17, 4, 2017}};*/
     int i;
-    int j;
-    int mayor;
-    int encontrado;
-    int legajoAux;
-    int edadAux;
 
-    for (i=0 ; i<MAX ; i++)
+    printf ("Datos de unAlumno:\n\n");
+    for (i=0 ; i<3 ; i++)
     {
-        printf ("Ingrese un numero de legajo: ");
-        scanf ("%d", &legajoAux);
-
-        printf ("Ingrese un salario: ");
-        scanf ("%f", &salarioAux);
-
-        printf ("ingrese una edad: ");
-        scanf ("%d", &edadAux);
-
-        salario [i] = salarioAux;
-        edad [i] = edadAux;
-        legajo [i] = legajoAux;
-
-        salario [j] = salarioAux;
-        edad [j] = edadAux;
-        legajo [j] = legajoAux;
-
-    }
-    if (encontrado == 0)
-        {
-            printf ("registro no encontrado");
-        }
-
-        system ("cls");
-
-    printf("\n\nLegajo\tEdad\tSalario");
-
-
-
-    for (i=0 ; i<MAX ; i++)
-    {
-        printf ("\n %d\t%d\t%f \n", legajo[i],edad[i],salario[i]);
-
-        if (i == 0 || salario[i] > mayor)
-        {
-            mayor = salario [i];
-        }
-
-        if (legajoAux == legajo [i])
-        {
-            printf ("\n %d\t%d\t%f \n", legajo[i],edad[i],salario[i]);
-            encontrado = 1;
-        }
-
+        mostrarAlumno(curso[i]);
     }
 
-    for (i=0 ; i< MAX-1 ; i++)
+    /*printf("Ingrese un legajo: ");
+    scanf ("%d", &unAlumno.legajo);
+
+    printf("Ingrese nombre: ");
+    fflush(stdin);
+    gets(unAlumno.nombre);
+
+    printf("Ingrese apellido: ");
+    fflush(stdin);
+    scanf("%s", unAlumno.apellido);
+
+    printf("Ingrese promedio: ");
+    scanf("%f", &unAlumno.promedio);
+
+    printf("Ingrese dia de nacimiento: ");
+    scanf("%d", &unAlumno.fnac.dia);
+
+    printf("Ingrese mes de nacimiento: ");
+    scanf("%d", &unAlumno.fnac.mes);
+
+    printf("Ingrese anio de nacimiento: ");
+    scanf("%d", &unAlumno.fnac.anio);
+
+
+    printf ("Datos de unAlumno:\n\n");
+    mostrarAlumno(unAlumno);*/
+
+    /*unAlumno.legajo = 827;
+    strcpy(unAlumno.nombre, "Juan");
+    strcpy(unAlumno.apellido, "Perez");
+    unAlumno.promedio = 7;*/
+
+
+
+    return (0);
+}
+
+void mostrarAlumno (eAlumno alumno)
+{
+    printf("Datos del alumno\n\n");
+    printf("Legajo: %d\n" ,alumno.legajo);
+    printf("Nombre %s %s\n" ,alumno.nombre, alumno.apellido);
+    printf("Promedio: %.2f\n", alumno.promedio);
+    printf("Fecha Nacimiento: %d/%d/%d\n\n", alumno.fnac.dia, alumno.fnac.mes, alumno.fnac.anio);
+}
+
+void mostrarCurso (curso[3], int cantidad)
+{
+    int i;
+
+    for (i=0 ; i<cantidad ; i++)
     {
-        for (j=i+1 ; j<MAX ; j++)
-            {
-                if (legajo[i] > legajo [j])
-                {
-                    legajoAux = legajo [i];
-                    legajo [i] = legajo [j];
-                    legajo [j] = legajoAux;
-
-                    edadAux = edad [i];
-                    edad [i] = edad [j];
-                    edad [j] = edadAux;
-
-                    salarioAux = salario [i];
-                    salario [i] = salario[j];
-                    salario [j] = salarioAux;
-
-                }
-            }
-
+        mostrarAlumno(curso[i]);
     }
-
-
-
-        printf ("el salario mayor es %d: ", mayor);
-
-
-    return 0;
 }
